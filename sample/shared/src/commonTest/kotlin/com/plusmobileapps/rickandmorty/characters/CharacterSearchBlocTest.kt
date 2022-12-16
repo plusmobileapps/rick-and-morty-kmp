ef8db9e005e8ecf685b64ca18f7ec598d51b0bf5
Andrew Steinmetz <andrew@plusmobileapps.com>
2022-12-16 00:45:33 -0800

    create some more tests for search and clean up other tests

--- sample/shared/src/commonTest/kotlin/com/plusmobileapps/rickandmorty/characters/CharacterSearchBlocTest.kt
+++ sample/shared/src/commonTest/kotlin/com/plusmobileapps/rickandmorty/characters/CharacterSearchBlocTest.kt
@@ -2,6 +2,7 @@ package com.plusmobileapps.rickandmorty.characters
 
 import com.plusmobileapps.paging.PagingDataSourceState
 import com.plusmobileapps.rickandmorty.TestAppComponentContext
+import com.plusmobileapps.rickandmorty.characters.CharacterMocks.pickleRick
 import com.plusmobileapps.rickandmorty.characters.search.CharacterSearchBloc
 import com.plusmobileapps.rickandmorty.characters.search.CharacterSearchBloc.Output
 import com.plusmobileapps.rickandmorty.characters.search.CharacterSearchBlocImpl
@@ -12,8 +13,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.TestScope
 import org.kodein.mock.Mock
 import org.kodein.mock.tests.TestsWithMocks
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 class CharacterSearchBlocTest : TestsWithMocks() {
@@ -21,37 +20,92 @@ class CharacterSearchBlocTest : TestsWithMocks() {
     override fun setUpMocks() = injectMocks(mocker)
 
     @Mock
-    lateinit var output: (CharacterSearchBloc.Output) -> Unit
+    lateinit var output: (Output) -> Unit
 
     @Mock
     lateinit var useCase: CharacterSearchUseCase
 
-    private val outputs = mutableListOf<Output>()
 
-    private fun TestScope.createBloc(context: TestAppComponentContext): CharacterSearchBloc =
+    private fun TestAppComponentContext.createBloc(): CharacterSearchBloc =
         CharacterSearchBlocImpl(
-            componentContext = context,
+            componentContext = this,
             output = { output(it) },
             useCase = useCase,
         )
 
-    @BeforeTest
-    fun setup() {
-        every { output.invoke(isAny(outputs)) } returns Unit
-    }
-
-    @AfterTest
-    fun tearDown() {
-        outputs.clear()
+    @Test
+    fun WHEN_backClick_THEN_outputIsGoBack() = runBlocTest {
+        val outputs = expectOutputs()
+        it.createBloc().onBackClicked()
+        outputs.first() shouldBe Output.GoBack
     }
 
     @Test
-    fun backClickShouldEmitOutput() = runBlocTest {
-        every { useCase.pageLoaderState } returns MutableStateFlow(PagingDataSourceState())
-        every { output.invoke(isAny()) }
-        val bloc = createBloc(it)
-        bloc.onBackClicked()
-        outputs.first() shouldBe Output.GoBack
+    fun WHEN_characterClicked_THEN_outputIsOpenCharacter() = runBlocTest {
+        val outputs = expectOutputs()
+        it.createBloc().onCharacterClicked(pickleRick)
+        outputs.first() shouldBe Output.OpenCharacter(pickleRick.id)
+    }
+
+    @Test
+    fun WHEN_searchClicked_THEN_searchIsInitiated() = runBlocTest {
+        every { useCase.loadFirstPage(QUERY, null, "", null) } returns Unit
+        mockPageLoader()
+
+        it.createBloc().apply {
+            onQueryChanged(QUERY)
+            onSearchClicked()
+        }
+
+        verify {
+            useCase.pageLoaderState
+            useCase.loadFirstPage(
+                query = QUERY,
+                status = null,
+                species = "",
+                gender = null,
+            )
+        }
+    }
+
+    @Test
+    fun WHEN_firstPageTryAgainClicked_THEN_nextPageLoaded() = runBlocTest {
+        every { useCase.loadFirstPage(QUERY, null, "", null) } returns Unit
+        mockPageLoader()
+
+        it.createBloc().apply {
+            onQueryChanged(QUERY)
+            onFirstPageTryAgainClicked()
+        }
+
+        verify {
+            useCase.pageLoaderState
+            useCase.loadFirstPage(
+                query = QUERY,
+                status = null,
+                species = "",
+                gender = null,
+            )
+        }
+    }
+
+    private fun expectOutputs(): List<Output> {
+        mockPageLoader()
+        val outputs = mutableListOf<Output>()
+        every { output.invoke(isAny(outputs)) } returns Unit
+        return outputs
+    }
+
+    private fun mockPageLoader(
+        mock: () -> MutableStateFlow<PagingDataSourceState<RickAndMortyCharacter>> = {
+            MutableStateFlow(PagingDataSourceState())
+        }
+    ) {
+        every { useCase.pageLoaderState } returns mock()
+    }
+
+    companion object {
+        const val QUERY = "Rick"
     }
 }
 
--- sample/shared/src/commonTest/kotlin/com/plusmobileapps/rickandmorty/characters/CharactersBlocTest.kt
+++ sample/shared/src/commonTest/kotlin/com/plusmobileapps/rickandmorty/characters/CharactersBlocTest.kt
@@ -1,24 +1,24 @@
 package com.plusmobileapps.rickandmorty.characters
 
+import com.plusmobileapps.paging.PageLoaderException
 import com.plusmobileapps.paging.PagingDataSourceState
 import com.plusmobileapps.rickandmorty.AppComponentContext
 import com.plusmobileapps.rickandmorty.characters.list.CharactersBloc
+import com.plusmobileapps.rickandmorty.characters.list.CharactersBloc.Output
 import com.plusmobileapps.rickandmorty.characters.list.CharactersBlocImpl
 import com.plusmobileapps.rickandmorty.runBlocTest
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.kodein.mock.Mock
 import org.kodein.mock.tests.TestsWithMocks
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class CharactersBlocTest : TestsWithMocks() {
 
     override fun setUpMocks() = injectMocks(mocker)
 
     @Mock
-    lateinit var output: (CharactersBloc.Output) -> Unit
+    lateinit var output: (Output) -> Unit
     @Mock
     lateinit var repository: CharactersRepository
 
@@ -30,21 +30,95 @@ class CharactersBlocTest : TestsWithMocks() {
         )
 
     @Test
-    fun pagingStateUpdatingShouldUpdateState() = runBlocTest {
-        val character = RickAndMortyCharacter(id = 4)
-        every { repository.pageLoaderState } returns MutableStateFlow(
-            PagingDataSourceState(
-                data = listOf(character)
-            )
-        )
+    fun GIVEN_emptyState_WHEN_pageLoaderStateUpdates_THEN_modelIsUpdated() = runBlocTest {
+        val pageLoader = MutableStateFlow(initialPagingState)
+        mockPageLoaderState { pageLoader }
 
         val bloc = it.createBloc()
 
         bloc.models.value shouldBe CharactersBloc.Model(
-            characters = listOf(character),
+            characters = listOf(),
             firstPageIsLoading = false,
             nextPageIsLoading = false,
             pageLoadedError = null,
+            hasMoreToLoad = false,
+        )
+
+        pageLoader.value = PagingDataSourceState(
+            isNextPageLoading = true,
+            isFirstPageLoading = true,
+            data = listOf(character),
+            pageLoaderError = PageLoaderException.NoNetworkException(true),
+            hasMoreToLoad = true,
+        )
+
+        bloc.models.value shouldBe CharactersBloc.Model(
+            characters = listOf(character),
+            firstPageIsLoading = true,
+            nextPageIsLoading = true,
+            pageLoadedError = PageLoaderException.NoNetworkException(true),
+            hasMoreToLoad = true,
+        )
+    }
+
+    @Test
+    fun WHEN_searchIsClicked_THEN_outputIsOpenCharacterSearch() = runBlocTest {
+        val outputs = expectOutputs()
+        mockPageLoaderState()
+
+        it.createBloc().onSearchClicked()
+
+        outputs.first() shouldBe Output.OpenCharacterSearch
+    }
+
+    @Test
+    fun WHEN_loadMoreCharacters_THEN_repositoryLoadNextPageIsCalled() = runBlocTest {
+        every { repository.loadNextPage() } returns Unit
+        mockPageLoaderState()
+
+        it.createBloc().loadMoreCharacters()
+
+        verify {
+            repository.pageLoaderState
+            repository.loadNextPage()
+        }
+    }
+
+
+    @Test
+    fun WHEN_characterClicked_THEN_outputIsOpenCharacter() = runBlocTest {
+        val outputs = expectOutputs()
+        mockPageLoaderState()
+
+        it.createBloc().onCharacterClicked(character)
+
+        outputs.first() shouldBe Output.OpenCharacter(character)
+    }
+
+    private fun mockPageLoaderState(
+        mock: () -> MutableStateFlow<PagingDataSourceState<RickAndMortyCharacter>> = {
+            MutableStateFlow(PagingDataSourceState())
+        }
+    ) {
+        every { repository.pageLoaderState } returns mock()
+    }
+
+    private fun expectOutputs(): List<Output> {
+        val outputs = mutableListOf<Output>()
+        every { output.invoke(isAny(outputs)) } returns Unit
+        return outputs
+    }
+
+    companion object {
+        val character = RickAndMortyCharacter(
+            id = 4,
+            name = "Pickle Rick"
+        )
+        val initialPagingState = PagingDataSourceState<RickAndMortyCharacter>(
+            isNextPageLoading = false,
+            isFirstPageLoading = false,
+            data = emptyList(),
+            pageLoaderError = null,
             hasMoreToLoad = false,
         )
     }
